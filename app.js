71f295253b2031610bbce72c560434498392bc27
10Ashish10 <akashish904@gmail.com>
2020-08-11 13:17:15 +0530

    AshishKr

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const mongoose =require('mongoose');
 
 const routes = require('./routes');
-const { ValidationError, NotFoundError } = require('./lib/errors');
+const { ValidationError,NotFoundError } = require('./lib/errors');
+mongoose.connect('mongodb://localhost:27017/petdb',{useNewUrlParser: true,useUnifiedTopology:true})
 
 const app = express();
+
+
+const db =mongoose.connection
+
+
+db.on('error',(err)=>
+    {
+         console.log(err)
+    })
+
+db.once('open',()=>    
+    {
+    console.log('MongoDB conection setup');
+    })
 
 app.use(express.json({ limit: '100kb' }));
 app.use('/', routes);
 app.use('/', (err, req, res, next) => {
-  // default to 500 internal server error unless we've defined a specific error
+  
   let code = 500;
   if (err instanceof ValidationError) {
     code = 400;
@@ -20,5 +36,11 @@ app.use('/', (err, req, res, next) => {
     message: err.message,
   });
 });
+
+
+app.listen(5002, function(req,res){        
+
+  console.log("Server working on port 5002");
+})
 
 module.exports = app;
